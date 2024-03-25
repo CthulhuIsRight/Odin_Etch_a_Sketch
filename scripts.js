9c0ca0a763e582d778433328a6a85add6280273e
CthulhuIsRight <7577221+CthulhuIsRight@users.noreply.github.com>
2024-03-25 16:18:45 -0700

    Added google icons, enabled grid function for canvas

--- scripts.js
+++ scripts.js
@@ -1,27 +1,41 @@
-const GRIDSIDE = 600;
-let squaresPerSide = 16;
+const gridWidth = getComputedStyle(document.body).getPropertyValue("--grid-width");
+const accentColor = getComputedStyle(document.body).getPropertyValue("--accent-color");
+const inactiveColor = getComputedStyle(document.body).getPropertyValue("--inactive-color");
 
+const container = document.querySelector(".container");
 const sketchArea = document.querySelector("#sketch-area");
-const sliderContainer = document.querySelector("#slider-container")
 const slider = document.querySelector("#slider");
 const sliderValue = document.querySelector("#slider-value");
-sliderValue.textContent = `${slider.value} x ${slider.value} (Resolution)`;
-sketchArea.style.width = sketchArea.style.height = `${GRIDSIDE}px`;
+
+const gridToggle = document.querySelector("#grid-toggle")
+let squaresPerSide = 16;
+let gridVisible = false;
+
+function toggleGrid(){
+    gridVisible = gridVisible ? false : true;
+    gridToggle.style.color = gridVisible ? accentColor : inactiveColor;
+    removeGridCells();
+    createGridCells();
+}
 
 function setBackgroundColor(){
     this.style.backgroundColor = "black";
 }
 
-function createGridCells(squaresPerSide){
+function createGridCells(){
     const numOfSquares = (squaresPerSide * squaresPerSide);
-    const widthOrHeight = `${(GRIDSIDE/squaresPerSide)-2}px`;
-
-    for(let i = 0; i < numOfSquares; i++){
+    for (let i = 0; i < numOfSquares; i++){
         const gridCell = document.createElement("div");
+        if(gridVisible){
+            widthOrHeight = `${(parseInt(gridWidth)/squaresPerSide) -2}px`;
+            gridCell.style.border = "1px solid whitesmoke";
+        } else if (!gridVisible){
+            widthOrHeight = `${(parseInt(gridWidth)/squaresPerSide)}px`;
+            gridCell.style.border = "none";
+        }
         gridCell.style.width = gridCell.style.height = widthOrHeight;
-        gridCell.classList.add("cell");
-        sketchArea.appendChild(gridCell);
         gridCell.addEventListener("mouseover", setBackgroundColor);
+        sketchArea.appendChild(gridCell);
     }
 }
 
@@ -32,10 +46,11 @@ function removeGridCells(){
 }
 
 slider.oninput = function(){
-    let txt = `${this.value} x ${this.value} (Resolution)`;
-    sliderValue.innerHTML = txt;
+    squaresPerSide = this.value;
+    sliderValue.textContent = `${this.value} x ${this.value} (Resolution)`;
     removeGridCells();
-    createGridCells(this.value);
+    createGridCells();
 }
 
-createGridCells(16);
+gridToggle.addEventListener("click", toggleGrid);
+createGridCells();
